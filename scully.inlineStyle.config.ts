b4846d55dbf97ce591986ad366438dc6e838ad99
Hassan Sani <saniyhassan@gmail.com>
2020-06-11 01:06:08 +0100

    making a scully page

---
+++ scully.inlineStyle.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: './src',
+  projectName: 'inlineStyle',
+  outDir: './dist/static',
+  routes: {
+  }
+};
