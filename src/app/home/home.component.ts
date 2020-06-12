4c3f92dadeb05ee647d152121b8d91178135aa2c
Hassan Sani <saniyhassan@gmail.com>
2020-06-12 04:16:21 +0100

    working on fontawesome

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faTwitter, faDev, faLinkedin, faGithub, faMedium } from '@fortawesome/free-brands-svg-icons'
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  twitter = faTwitter;
+  github = faGithub;
+  linkedin = faLinkedin;
+  dev = faDev;
+  medium = faMedium;
 
   constructor() { }
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FontAwesomeModule
   ]
 })
 export class HomeModule { }
